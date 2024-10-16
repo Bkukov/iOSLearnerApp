809a06c8e5bb19519e91de07eb7e815a42e03c8d
Boyan Kukov <bkukov@gmail.com>
2024-10-16 16:47:04 -0400

    Initial Commit

---
+++ R&M/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  R&M
+//
+//  Created by bkukov on 10/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ R&MTests/R_MTests.swift
@@ -0,0 +1,17 @@
+//
+//  R_MTests.swift
+//  R&MTests
+//
+//  Created by bkukov on 10/16/24.
+//
+
+import Testing
+@testable import R_M
+
+struct R_MTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
